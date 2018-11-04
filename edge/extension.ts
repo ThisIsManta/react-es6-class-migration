bd661e58e706d5bd70a5cdb3ffa69a95665bd18e
Anantachai Saothong (Manta) <thisismanta@outlook.com>
2018-11-04 18:01:23 +0700

    Added a dialog asking if users want to convert the file to TypeScript React

--- edge/extension.ts
+++ edge/extension.ts
@@ -1,3 +1,6 @@
+import * as fs from 'fs'
+import * as fp from 'path'
+import * as _ from 'lodash'
 import * as vscode from 'vscode'
 import migrateReactClass from './migrateReactClass'
 import migrateTypeDefinition from './migrateTypeDefinition'
@@ -9,12 +12,14 @@ export function activate(context) {
 			return vscode.window.showErrorMessage('No document opened.')
 		}
 
-		const document = editor.document
+		const { document } = editor
 		try {
 			const originalCode = document.getText()
 			let modifiedCode = migrateReactClass(originalCode, document.languageId === 'typescriptreact' ? 'tsx' : 'jsx')
 
-			if (document.languageId === 'typescriptreact') {
+			const currentFileIsTypeScript = document.languageId === 'typescriptreact'
+			const currentFileWilBeRenamedToTypeScript = !currentFileIsTypeScript && await checkIfUserWantsToConvertToTypeScript()
+			if (currentFileIsTypeScript || currentFileWilBeRenamedToTypeScript) {
 				modifiedCode = migrateTypeDefinition(modifiedCode)
 			}
 
@@ -27,11 +32,42 @@ export function activate(context) {
 				await vscode.commands.executeCommand('editor.action.formatDocument')
 			}
 
+			if (currentFileWilBeRenamedToTypeScript) {
+				vscode.window.withProgress({ title: 'Converting to TypeScript React', location: vscode.ProgressLocation.Notification, cancellable: true }, async (progress, cancellationToken) => {
+					await document.save()
+					
+					if (cancellationToken.isCancellationRequested) {
+						return
+					}
+
+					const newFilePath = document.fileName.replace(new RegExp(_.escapeRegExp(fp.extname(document.fileName)) + '$'), '.tsx')
+					fs.renameSync(document.fileName, newFilePath)
+
+					if (cancellationToken.isCancellationRequested) {
+						fs.renameSync(newFilePath, document.fileName)
+						return
+					}
+
+					await vscode.commands.executeCommand('workbench.action.closeActiveEditor')
+					await vscode.window.showTextDocument(await vscode.workspace.openTextDocument(newFilePath), editor.viewColumn)
+				})
+			}
+
 		} catch (error) {
 			vscode.window.showErrorMessage(error.message)
 			console.error(error)
 		}
 	}))
+}
+
+async function checkIfUserWantsToConvertToTypeScript() {
+	const typeScriptReactFile = await vscode.workspace.findFiles('**/*.tsx', null, 1)
+	if (typeScriptReactFile.length === 0) {
+		return false
+	}
+
+	const select = await vscode.window.showInformationMessage('The current workspace contains a TypeScript React (*.tsx) file.', { modal: true }, 'Convert this file to TypeScript React')
+	return !!select
 }
 
 export function deactivate() { }
